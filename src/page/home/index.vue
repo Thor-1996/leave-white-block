fddb455fd14e8d52efc57f38ab30305d4cf41d12
zhaojian <15073245605@163.com>
2020-01-15 10:56:33 +0800

    加入大杂烩

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="home">
     <h1>欢迎来到webliker.cn,<router-link to='/game'>点我</router-link>来玩别踩白块</h1>
+    <div @click="showHodgepodge = !showHodgepodge" class="click-tip">点我一下？</div>
+    <div class="Hodgepodge" v-show="showHodgepodge">
+      <window-width/>
+    </div>
   </div>
 </template>
 
 <script>
+import WindowWidth from './windowWidth'
 export default {
   name: 'home',
+  components:{
+    WindowWidth
+  },
+  data() {
+    return {
+      showHodgepodge: false
+    }
+  }
 }
 
 </script>
 <style lang="stylus" scoped>
   @import './index'
+  .click-tip{
+    font-size: 8px;
+  }
 </style>
---
+++ src/page/home/windowWidth.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+   屏幕宽度：{{ width }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'windowWidth',
+  data(){
+    return {
+      width: 0
+    }
+  },
+  mounted(){
+    this.width = innerWidth
+    window.addEventListener('resize',()=>{
+      this.width = innerWidth
+    })
+  }
+}
+</script>
