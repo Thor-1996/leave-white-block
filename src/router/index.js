1b3d8701f1a69df09d6427cc227bfe570cfbf374
Thor <15073245605@163.com>
2020-09-24 16:53:12 +0800

    路由设为哈希模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import game from '@/page/game'
-import home from '@/page/home'
+import Vue from "vue";
+import Router from "vue-router";
+import game from "@/page/game";
+import home from "@/page/home";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode:'history',
+  // mode:'history',
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: home,
+      path: "/",
+      name: "home",
+      component: home
     },
     {
-      path: '/game',
-      name: 'game',
-      component: game,
+      path: "/game",
+      name: "game",
+      component: game
     }
   ]
-})
+});
